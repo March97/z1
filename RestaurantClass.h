bb768d5fe4caf8c7526c5625d3e5696934bcfbbb
March97 <dominikm97@wp.pl>
2017-03-14 18:48:28 +0100

    ver 0.01
    naglowek klasy

---
+++ RestaurantClass.h
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class Restaurant
+{
+private:
+    string name; //nazwa restauracji
+    string owner; // nazwisko wlasciciela
+    string kindoffood; // rodzaj kuchni
+    int seats; // ilosc miejsc
+    int employees; // ilosc pracownikow
+    float incomes; // przychody
+    float surface; // powierzchni lokalu
+
+public:
+    Restaurant(string, string, string);
+    ~Restaurant();
+};
