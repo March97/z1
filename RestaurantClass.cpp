556c499baefa569682222d2620bc8aafeaeaf2e3
March97 <dominikm97@wp.pl>
2017-03-14 19:02:50 +0100

    ver0.02
    konstruktor i destruktor

--- RestaurantClass.cpp
+++ RestaurantClass.cpp
@@ -2,3 +2,15 @@
 #include "RestaurantClass.h"
 
 using namespace std;
+
+Restaurant::Restaurant(string n, string o, string k)
+{
+    name=n;
+    owner=o;
+    kindoffood=k;
+}
+
+Restaurant::~Restaurant()
+{
+    cout<<"Destruktor"<<endl;
+}
